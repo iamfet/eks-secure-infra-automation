7e7ff17c6e468fc54bb429007626e609cf629c14
iamfet <fetuyee@gmail.com>
2025-06-12 20:12:00 +0000

    infra(ci): add terraform destroy action

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "public_subnets_cidr" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "fet-boutique"
+  default     = "carney-shop"
 }
 
 variable "cluster_version" {
