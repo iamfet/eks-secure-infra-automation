b0140c8b5e578330d23012d44f64363a7deed3c3
iamfet <fetuyee@gmail.com>
2025-06-22 12:51:47 +0000

    ci: trigger ci

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "eks" {
     dev = {
       instance_types = ["m5.xlarge"]
       min_size       = 1
-      max_size       = 5
+      max_size       = 3
       desired_size   = 2
     }
   }
--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "public_subnets_cidr" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "joana-shop"
+  default     = "carney-boutique"
 }
 
 variable "cluster_version" {
