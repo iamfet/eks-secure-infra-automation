3db0ef2da215cfa73d95e646c5f02a4f84af10bb
iamfet <fetuyee@gmail.com>
2025-06-22 19:21:05 +0000

    ci: trigger ci

--- kube-resources.tf
+++ kube-resources.tf
@@ -1,17 +1,12 @@
-data "aws_eks_cluster" "cluster" {
-  name       = "${var.project_name}-eks-cluster"
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name       = "${var.project_name}-eks-cluster"
-  depends_on = [module.eks]
-}
-
 provider "kubernetes" {
-  host                   = try(data.aws_eks_cluster.cluster.endpoint, "")
-  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), "")
-  token                  = try(data.aws_eks_cluster_auth.cluster.token, "")
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 resource "kubernetes_namespace" "online-boutique" {
--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "public_subnets_cidr" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "carney-boutique"
+  default     = "ama-shop"
 }
 
 variable "cluster_version" {
