046578ee45949e92ae014b8285fb00bfbc81ed78
iamfet <fetuyee@gmail.com>
2025-06-23 16:41:49 +0000

    ci: trigger ci

--- kube-resources.tf
+++ kube-resources.tf
@@ -10,7 +10,7 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_namespace" "online-boutique" {
-  depends_on = [module.eks, module.vpc, module.eks_blueprints_addons]
+  depends_on = [module.eks, module.vpc]
   metadata {
     name = "online-boutique"
   }
