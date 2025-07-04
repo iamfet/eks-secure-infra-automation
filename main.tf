b11575b5b81473f27eb426b541f223438ad68b13
iamfet <fetuyee@gmail.com>
2025-07-04 18:46:10 +0000

    ci: trigger ci

--- main.tf
+++ main.tf
@@ -115,8 +115,8 @@ module "eks_blueprints_addons" {
 
   enable_aws_load_balancer_controller = true
   enable_metrics_server               = true
-  /* enable_external_secrets             = true */
-  enable_cluster_autoscaler = true
+  enable_external_secrets             = true
+  enable_cluster_autoscaler           = true
 
   cluster_autoscaler = {
     set = [
