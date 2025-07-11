fc5cc72c3277fd2f30774e2f415c4577203ec1d4
iamfet <fetuyee@gmail.com>
2025-07-11 09:38:29 +0000

    chore: trigger ci

--- istio.tf
+++ istio.tf
@@ -58,6 +58,14 @@ resource "aws_vpc_security_group_ingress_rule" "istio-gateway-lb_https" {
   to_port           = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "istio-gateway-lb_healthcheck" {
+  security_group_id = aws_security_group.istio-gateway-lb.id
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  from_port         = 15021
+  ip_protocol       = "tcp"
+  to_port           = 15021
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.istio-gateway-lb.id
   cidr_ipv4         = "0.0.0.0/0"
