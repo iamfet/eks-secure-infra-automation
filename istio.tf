5754580b329aac61d78702e105786469714345f9
iamfet <fetuyee@gmail.com>
2025-07-14 17:22:51 +0000

    chore: trigger ci

--- istio.tf
+++ istio.tf
@@ -6,7 +6,7 @@ resource "helm_release" "istio-base" {
   version          = "1.26.2"
   create_namespace = true
   namespace        = "istio-system"
-  depends_on       = [module.eks_blueprints_addons]
+  depends_on       = [module.eks, helm_release.aws-load-balancer-controller]
 }
 
 # 2. Install istiod second (control plane)
