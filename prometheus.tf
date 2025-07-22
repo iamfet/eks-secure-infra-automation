ae783729e0d096ff204680c6fd9bd9d4e985f393
iamfet <fetuyee@gmail.com>
2025-07-22 17:43:45 +0000

    feat: add prometheus

--- prometheus.tf
+++ prometheus.tf
@@ -1,11 +1,11 @@
 resource "helm_release" "istio_prometheus" {
-  name       = "kube-prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  version    = "75.12.0"
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "75.12.0"
   create_namespace = true
-  namespace  = "monitoring"
-  depends_on = [helm_release.istiod]
+  namespace        = "monitoring"
+  depends_on       = [helm_release.istiod]
   values = [
     <<-EOF
     prometheus:
