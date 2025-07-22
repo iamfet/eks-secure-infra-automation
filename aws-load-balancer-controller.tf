8fc5b3de1b0f7128f7e0525a7e59a885e91deaa7
Frederick Ebenezer Tuyee <fetuyee@gmail.com>
2025-07-22 20:28:36 +0000

    refactor: update aws-load-balancer-controller.tf

--- aws-load-balancer-controller.tf
+++ aws-load-balancer-controller.tf
@@ -1,14 +1,16 @@
-module "aws_load_balancer_controller_irsa" {
+#Create the IAM Role for Service Account for cluster autoscaler
+module "cluster_autoscaler_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.59"
 
-  role_name                              = "${var.project_name}-aws-load-balancer-controller-irsa"
-  attach_load_balancer_controller_policy = true
+  role_name                         = "${var.project_name}-cluster-autoscaler-irsa"
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_names = [module.eks.cluster_name]
 
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
     }
   }
 
@@ -18,36 +20,64 @@ module "aws_load_balancer_controller_irsa" {
   }
 }
 
-resource "helm_release" "aws-load-balancer-controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  version    = "1.13.3"
+
+resource "helm_release" "cluster_autoscaler" {
+  name       = "cluster-autoscaler"
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+  version    = "9.48.0"
   namespace  = "kube-system"
-  depends_on = [module.eks, module.aws_load_balancer_controller_irsa]
 
-  wait = true
-
-  set = [
-    {
-      name  = "clusterName"
-      value = module.eks.cluster_name
-    },
-    {
-      name  = "vpcId"
-      value = module.vpc.vpc_id
-    },
-    {
-      name  = "serviceAccount.create"
-      value = "true"
-    },
-    {
-      name  = "serviceAccount.name"
-      value = "aws-load-balancer-controller"
-    },
-    {
-      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-      value = module.aws_load_balancer_controller_irsa.iam_role_arn
-    }
+  depends_on = [
+    module.eks,
+    helm_release.aws-load-balancer-controller,
+    module.cluster_autoscaler_irsa
   ]
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.aws_region
+  }
+
+  # Optional tuning
+  set {
+    name  = "extraArgs.scale-down-unneeded-time"
+    value = "2m"
+  }
+
+  set {
+    name  = "extraArgs.skip-nodes-with-local-storage"
+    value = "false"
+  }
+
+  set {
+    name  = "extraArgs.skip-nodes-with-system-pods"
+    value = "false"
+  }
+
+  # IRSA-related configuration
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "cluster-autoscaler"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.cluster_autoscaler_irsa.iam_role_arn
+  }
 }
