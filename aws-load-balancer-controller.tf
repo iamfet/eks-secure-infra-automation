8d0c70db52dd55d16f655db1848df21c404e4227
Frederick Ebenezer Tuyee <fetuyee@gmail.com>
2025-07-22 21:01:55 +0000

    Update aws-load-balancer-controller.tf

--- aws-load-balancer-controller.tf
+++ aws-load-balancer-controller.tf
@@ -1,11 +1,10 @@
-#Create the IAM Role for Service Account for cluster autoscaler
 module "cluster_autoscaler_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.59"
 
   role_name                        = "${var.project_name}-cluster-autoscaler-irsa"
   attach_cluster_autoscaler_policy = true
-  cluster_autoscaler_cluster_names = [module.eks.cluster_name]
+  cluster_autoscaler_cluster_names = ["${var.project_name}-eks-cluster"]
 
   oidc_providers = {
     main = {
@@ -15,69 +14,52 @@ module "cluster_autoscaler_irsa" {
   }
 
   tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    "Environment" = "dev"
+    "Terraform"   = "true"
   }
 }
 
 
-resource "helm_release" "cluster_autoscaler" {
+resource "helm_release" "cluster-autoscaler" {
   name       = "cluster-autoscaler"
   repository = "https://kubernetes.github.io/autoscaler"
   chart      = "cluster-autoscaler"
   version    = "9.48.0"
   namespace  = "kube-system"
+  depends_on = [module.eks, helm_release.aws-load-balancer-controller, module.cluster_autoscaler_irsa]
 
-  depends_on = [
-    module.eks,
-    helm_release.aws-load-balancer-controller,
-    module.cluster_autoscaler_irsa
+  set = [
+    {
+      name  = "autoDiscovery.clusterName"
+      value = module.eks.cluster_name
+    },
+    {
+      name  = "awsRegion"
+      value = var.aws_region
+    },
+    {
+      name  = "extraArgs.scale-down-unneeded-time"
+      value = "2m"
+    },
+    {
+      name  = "extraArgs.skip-nodes-with-local-storage"
+      value = "false"
+    },
+    {
+      name  = "extraArgs.skip-nodes-with-system-pods"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "cluster-autoscaler"
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.cluster_autoscaler_irsa.iam_role_arn
+    }
   ]
-
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = module.eks.cluster_name
-  }
-
-  set {
-    name  = "awsRegion"
-    value = var.aws_region
-  }
-
-  # Optional tuning
-  set {
-    name  = "extraArgs.scale-down-unneeded-time"
-    value = "2m"
-  }
-
-  set {
-    name  = "extraArgs.skip-nodes-with-local-storage"
-    value = "false"
-  }
-
-  set {
-    name  = "extraArgs.skip-nodes-with-system-pods"
-    value = "false"
-  }
-
-  # IRSA-related configuration
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
-
-  set {
-    name  = "rbac.serviceAccount.create"
-    value = "true"
-  }
-
-  set {
-    name  = "rbac.serviceAccount.name"
-    value = "cluster-autoscaler"
-  }
-
-  set {
-    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.cluster_autoscaler_irsa.iam_role_arn
-  }
 }
