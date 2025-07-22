a4a90c0abb1158d271701e3fe2eb69ba2f6a83c3
iamfet <fetuyee@gmail.com>
2025-07-22 13:43:08 +0000

    chore: trigger ci

--- prometheus.tf
+++ prometheus.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "prometheus"
-  version          = "27.28"
+  version          = "27.28.0"
   namespace        = "monitoring"
   create_namespace = true
   depends_on       = [module.eks, helm_release.aws-load-balancer-controller]
