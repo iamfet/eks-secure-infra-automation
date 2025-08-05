33032b9664b7bb3c7c2d543cfdc00df2fdd2b5b9
iamfet <fetuyee@gmail.com>
2025-08-05 20:32:45 +0000

    chore: trigger ci

--- defectdojo.tf
+++ defectdojo.tf
@@ -2,6 +2,7 @@ resource "helm_release" "defectdojo" {
   name       = "defectdojo"
   repository = "https://defectdojo.github.io/django-DefectDojo"
   chart      = "defectdojo"
+  version    = "1.6.153"
   namespace  = "defectdojo"
   depends_on = [module.eks, helm_release.istio-ingressgateway, kubernetes_namespace.defectdojo]
 
