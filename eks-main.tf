41bd1bc126ad32a1935e27693231dac2aa30503b
iamfet <fetuyee@gmail.com>
2025-08-08 22:55:56 +0000

    chore: trigger ci

--- eks-main.tf
+++ eks-main.tf
@@ -59,7 +59,8 @@ module "eks" {
     kube-proxy             = {}
     vpc-cni                = {}
     aws-ebs-csi-driver = {
-      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+      most_recent = true
+      resolve_conflicts_on_create = "OVERWRITE"
     }
   }
 
@@ -146,20 +147,7 @@ module "eks" {
   }
 }
 
-# EBS CSI Driver IRSA
-module "ebs_csi_irsa_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name             = "${var.project_name}-ebs-csi"
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-}
 
 
 
