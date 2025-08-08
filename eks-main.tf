a1dad225b59454eb882852b514a9bcc3cb8b2ad8
iamfet <fetuyee@gmail.com>
2025-08-08 19:50:03 +0000

    Reapply "refactor: update config"
    This reverts commit dc0623bce9c511c1d258c0b2ad7d61ab8b42fdcd.

--- eks-main.tf
+++ eks-main.tf
@@ -103,9 +103,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     dev = {
-      instance_types = ["t2.large"]
+      instance_types = ["t2.xlarge"]
       min_size       = 1
-      max_size       = 6
+      max_size       = 5
       desired_size   = 3
 
       tags = {
