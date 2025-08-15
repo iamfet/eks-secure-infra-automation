64331adb4db621a407367540511b0f3144d426dd
iamfet <fetuyee@gmail.com>
2025-08-15 09:15:37 +0000

    chore: trigger ci

--- eks-main.tf
+++ eks-main.tf
@@ -52,8 +52,6 @@ module "eks" {
 
   endpoint_public_access = true
 
-  depends_on = [module.vpc]
-
   addons = {
     coredns                = {}
     eks-pod-identity-agent = { before_compute = true }
